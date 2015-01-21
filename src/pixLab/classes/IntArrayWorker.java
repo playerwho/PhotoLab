a76dd56b3d77cb8bf6970919a316be95c6d5fb95
Austin Widmeier <austinwidmeier@gmail.com>
2015-01-21 13:40:00 -0700

    photoooooooossssssss
    sssssssssssssssssssssss

---
+++ src/pixLab/classes/IntArrayWorker.java
@@ -0,0 +1,133 @@
+package pixLab.classes;
+
+public class IntArrayWorker
+{
+	/** two dimensional matrix */
+	private int[][] matrix = null;
+
+	/**
+	 * set the matrix to the passed one
+	 * 
+	 * @param theMatrix
+	 *            the one to use
+	 */
+	public void setMatrix(int[][] matrix)
+	{
+		this.matrix = matrix;
+	}
+
+	/**
+	 * Method to return the total
+	 * 
+	 * @return the total of the values in the array
+	 */
+	public int getTotal()
+	{
+		int total = 0;
+		for (int row = 0; row < matrix.length; row++)
+		{
+			for (int col = 0; col < matrix[0].length; col++)
+			{
+				total += matrix[row][col];
+			}
+		}
+		return total;
+	}
+
+	/**
+	 * Method to return the total using a nested for-each loop
+	 * 
+	 * @return the total of the values in the array For-each loops always has rows first
+	 */
+	public int getTotalNested()
+	{
+		int total = 0;
+		for (int[] rowArray : matrix)
+		{
+			for (int item : rowArray)
+			{
+				total = total + item;
+			}
+		}
+		return total;
+	}
+
+	/**
+	 * Method to count the amount of rows and columns
+	 * 
+	 * @param searchValue
+	 * @return
+	 */
+	public int getCount(int searchValue)
+  {
+		 int count = 0;
+		 
+		 for(int [] row:matrix)
+		 {
+			 for(int spot:row)
+			 { 
+				 if(spot == searchValue)
+				 {
+					 count++;
+				 }
+			 }
+		 }
+		 
+		 return count; 
+  }
+	
+	public int getLargest()
+	{
+		
+		
+		return 0;
+	}
+	
+	public int getColTotal()
+	{
+		
+		
+		
+		return 0;
+	}
+
+	/**
+	 * print the values in the array in rows and columns
+	 */
+	public void print()
+	{
+		for (int row = 0; row < matrix.length; row++)
+		{
+			for (int col = 0; col < matrix[0].length; col++)
+			{
+				System.out.print(matrix[row][col] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	/**
+	 * fill the array with a pattern
+	 */
+	public void fillPattern1()
+	{
+		for (int row = 0; row < matrix.length; row++)
+		{
+			for (int col = 0; col < matrix[0].length; col++)
+			{
+				if (row < col)
+				{
+					matrix[row][col] = 1;
+				} else if (row == col)
+				{
+					matrix[row][col] = 2;
+				} else
+				{
+					matrix[row][col] = 3;
+				}
+			}
+		}
+	}
+
+}
